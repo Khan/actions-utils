df41658e52c975d41e4e2cf003d66857d870fa32
Jared Forsyth <jared@jaredforsyth.com>
2020-05-15 15:32:18 -0600

    restore get-base-ref for use in other actions

--- get-base-ref.js
+++ get-base-ref.js
@@ -1,7 +1,66 @@
 // @flow
+/**
+ * This is used to determine what the "base" branch for the current work is.
+ *
+ * - If the `GITHUB_BASE_REF` env variable is present, then we're running
+ *   under Github Actions, and we can just use that. If this is being run
+ *   locally, then it's a bit more tricky to determine the "base" for this
+ *   branch.
+ * - If this branch hasn't yet been pushed to github (e.g. the "upstream" is
+ *   something local), then use the upstream.
+ * - Otherwise, go back through the commits until we find a commit that is part
+ *   of another branch, that's either master, develop, or a feature/ branch.
+ *   TODO(jared): Consider using the github pull-request API (if we're online)
+ *   to determine the base branch.
+ */
+const { execSync } = require('child_process');
 
 const getBaseRef = async (head /*:string*/ = 'HEAD') => {
-    return `refs/remotes/origin/${process.env.GITHUB_BASE_REF}`;
+    const { GITHUB_BASE_REF } = process.env;
+    if (GITHUB_BASE_REF) {
+        if (GITHUB_BASE_REF.startsWith('refs/')) {
+            return GITHUB_BASE_REF;
+        } else {
+            return `refs/remotes/origin/${GITHUB_BASE_REF}`;
+        }
+    } else {
+        let upstream = execSync(
+            `git rev-parse --abbrev-ref '${head}@{upstream}'`,
+        );
+        upstream = upstream.trim();
+
+        // if upstream is local and not empty, use that.
+        if (upstream && !upstream.trim().startsWith('origin/')) {
+            return `refs/heads/${upstream}`;
+        }
+        let headRef = execSync(`git rev-parse --abbrev-ref ${head}`);
+        headRef = headRef.trim();
+        for (let i = 1; i < 100; i++) {
+            const stdout = execSync(
+                `git branch --contains ${head}~${i} --format='%(refname)'`,
+            );
+            let lines = stdout.split('\n').filter(Boolean);
+            lines = lines.filter((line) => line !== `refs/heads/${headRef}`);
+
+            // Note (Lilli): When running our actions locally, we want to be a little more
+            // aggressive in choosing a baseRef, going back to a shared commit on only `develop`,
+            // `master`, feature or release branches, so that we can cover more commits. In case,
+            // say, I create a bunch of experimental, first-attempt, throw-away branches that
+            // share commits higher in my stack...
+            for (const line of lines) {
+                if (
+                    line === 'refs/heads/develop' ||
+                    line === 'refs/heads/master' ||
+                    line.startsWith('refs/heads/feature/') ||
+                    line.startsWith('refs/heads/release/')
+                ) {
+                    return line;
+                }
+            }
+        }
+        // If all else fails, just use upstream
+        return `${head}@{upstream}`;
+    }
 };
 
 module.exports = getBaseRef;
