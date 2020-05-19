be05f93bf4397c670d99593d8a54c1e219e05c51
Jared Forsyth <jared@jaredforsyth.com>
2020-05-19 11:50:57 -0600

    Send a report even if everything passed
    This way we get a green check when landing!

--- send-report.js
+++ send-report.js
@@ -11,7 +11,7 @@
  * the GITHUB_TOKEN env variable.
  */
 
-const {GITHUB_TOKEN, GITHUB_WORKSPACE} = process.env;
+const { GITHUB_TOKEN, GITHUB_WORKSPACE } = process.env;
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
@@ -116,8 +116,8 @@ const githubReport = async (
     messages /*: Array<Message>*/,
 ) => {
     /* flow-uncovered-block */
-    const {GitHub, context} = require('@actions/github');
-    const {owner, repo} /*: {owner: string, repo: string}*/ = context.repo;
+    const { GitHub, context } = require('@actions/github');
+    const { owner, repo } /*: {owner: string, repo: string}*/ = context.repo;
     const client = new GitHub(token, {});
     const headSha = context.payload.pull_request.head.sha;
     const check = await client.checks.create({
@@ -127,7 +127,23 @@ const githubReport = async (
         name: title,
         head_sha: headSha,
     });
+    if (!messages.length) {
+        await client.checks.update({
+            owner,
+            repo,
+            check_run_id: check.data.id,
+            completed_at: new Date(),
+            status: 'completed',
+            conclusion: 'success',
+            output: {
+                title: title,
+                summary: `All clear!`,
+                annotations: [],
+            },
+        });
+    }
     /* end flow-uncovered-block */
+
     const annotations = messages.map((message) => ({
         path: removeWorkspace(message.path),
         start_line: message.start.line,
@@ -169,10 +185,6 @@ const githubReport = async (
 };
 
 const makeReport = (title /*: string*/, messages /*: Array<Message>*/) => {
-    if (!messages.length) {
-        console.log(`${title}: No errors`);
-        return;
-    }
     if (GITHUB_TOKEN) {
         return githubReport(title, GITHUB_TOKEN, messages);
     } else {
