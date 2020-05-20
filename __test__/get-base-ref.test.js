9de8f5aac2fb4ddc9e56e4ee3bd4f57c170ad61b
Lilli Szafranski <lilli@khanacademy.org>
2020-05-20 15:54:02 -0700

    Check fixes

--- __test__/get-base-ref.test.js
+++ __test__/get-base-ref.test.js
@@ -11,7 +11,17 @@ describe('validateBaseRef', () => {
             `refs/remotes/origin/an-example-remote-branch`,
         );
     });
-    it('should accempt a local branch', () => {
-        expect(validateBaseRef('master')).toEqual('master');
-    });
+
+    // When running on github, use what's in here, otherwise it was failing as it was
+    // seeing `master` as a remote branch,
+    const { GITHUB_BASE_REF } = process.env;
+    if (GITHUB_BASE_REF) {
+        it('should accept a local branch', () => {
+            expect(validateBaseRef(GITHUB_BASE_REF)).toEqual(GITHUB_BASE_REF);
+        });
+    } else {
+        it('should accept a local branch', () => {
+            expect(validateBaseRef('master')).toEqual('master');
+        });
+    }
 });
