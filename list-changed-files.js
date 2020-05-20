3529050672490bfe59608a8f29cc6b6dce46be78
Lilli Szafranski <lilli@khanacademy.org>
2020-05-20 15:40:06 -0700

    lint fix

--- list-changed-files.js
+++ list-changed-files.js
@@ -16,7 +16,7 @@ const run = async () => {
     const baseRef = await getBaseRef();
 
     // Note (Lilli): If baseRef is null for any reason, use `master` as opposed to failing the check silently
-    const files = await gitChangedFiles(baseRef ?? 'master', '.');
+    const files = await gitChangedFiles(baseRef ? baseRef : 'master', '.');
     files.forEach(file => console.log(file));
 
 };
